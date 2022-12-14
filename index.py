c65be4973a33ab84070d3633b9b9eff126473195
rizdaagisa <77386343+rizdaagisa@users.noreply.github.com>
2022-12-14 17:42:01 +0800

    Update index.py

--- index.py
+++ index.py
@@ -228,7 +228,7 @@ def index():
         else:
             return {'result' : 'failed'}
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+# if __name__ == '__main__':
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host="0.0.0.0", port=port)
     # app.run(debug=True)
